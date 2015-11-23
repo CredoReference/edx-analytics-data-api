cda41dcff891894ea84d65b19a3a8a25f0b77600
Dennis Jen <djen@edx.org>
2015-11-23 16:50:30 -0500

    added engagement models

--- analytics_data_api/learner_analytics/v0/models.py
+++ analytics_data_api/learner_analytics/v0/models.py
@@ -10,3 +10,32 @@ class RosterEntry(DocType):
      username = String(index='not_analyzed')
      class Meta:
           doc_type = 'roster_entry'
+
+
+class Engagement(models.Model):
+    """User interactions with entities within the courseware."""
+
+    course_id = models.CharField(db_index=True, max_length=255)
+    username = models.CharField(max_length=30)
+    date = models.DateTimeField()
+    entity_type = models.CharField(max_length=10)
+    entity_id = models.CharField(max_length=255)
+    event = models.CharField(max_length=30)
+    count = models.IntegerField()
+
+    class Meta(object):
+        db_table = 'engagement'
+
+class EngagementMetricRanges(models.Model):
+    """User interactions with entities within the courseware."""
+
+    course_id = models.CharField(db_index=True, max_length=255)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    metric = models.CharField(max_length=50)
+    range_type = models.CharField(max_length=50)
+    high_value = models.FloatField()
+    low_value = models.FloatField()
+
+    class Meta(object):
+        db_table = 'engagement_metric_ranges'
