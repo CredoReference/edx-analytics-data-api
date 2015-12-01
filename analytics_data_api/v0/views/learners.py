4f0735d2dd4f70274a93bfe412629652e1c76507
Daniel Friedman <dfriedman58@gmail.com>
2015-12-01 15:35:41 -0500

    Implement Learner List endpoint
    AN-6158

--- analytics_data_api/v0/views/learners.py
+++ analytics_data_api/v0/views/learners.py
@@ -7,9 +7,15 @@ from opaque_keys import InvalidKeyError
 from opaque_keys.edx.keys import CourseKey
 
 from analytics_data_api.v0.exceptions import (
-    CourseNotSpecifiedError, CourseKeyMalformedError, LearnerNotFoundError)
+    CourseKeyMalformedError,
+    CourseNotSpecifiedError,
+    LearnerNotFoundError,
+    ParameterValueError,
+)
+from analytics_data_api.constants import learner
 from analytics_data_api.v0.models import RosterEntry
-from analytics_data_api.v0.serializers import LearnerSerializer
+from analytics_data_api.v0.serializers import ElasticsearchDSLSearchSerializer, LearnerSerializer
+from analytics_data_api.v0.views.utils import split_query_argument
 
 
 class CourseViewMixin(object):
@@ -20,6 +26,8 @@ class CourseViewMixin(object):
     course_id = None
 
     def get(self, request, *args, **kwargs):
+        # TODO: We should check that the course_id exists at all in
+        # the data store.
         self.course_id = request.QUERY_PARAMS.get('course_id', None)
         if not self.course_id:
             raise CourseNotSpecifiedError()
@@ -64,7 +72,7 @@ class LearnerView(CourseViewMixin, generics.RetrieveAPIView):
 
     **Parameters**
 
-        You can specify course ID for which you want data.
+        You can specify the course ID for which you want data.
 
         course_id -- The course within which user data is requested.
 
@@ -85,3 +93,86 @@ class LearnerView(CourseViewMixin, generics.RetrieveAPIView):
         if len(queryset) == 1:
             return queryset[0]
         raise LearnerNotFoundError(username=self.username, course_id=self.course_id)
+
+
+class LearnerListView(CourseViewMixin, generics.ListAPIView):
+    """
+    Get a paginated list of student data for a particular course.
+
+    **Example Request**
+
+        GET /api/v0/learners/?course_id={course_id}
+
+    **Response Values**
+
+        TODO
+
+    **Parameters**
+
+        You can specify the course ID for which you want data.
+
+        TODO: add sorting/filtering
+
+        course_id -- The course within which user data is requested.
+        page -- The page of results which should be returned.
+        page_size -- The maximum number of results which should be returned per page
+        text_search -- A string to search over the name, username, and email of learners.
+        segments -- A comma-separated string of segments to which
+            learners should belong.  Semgents are "OR"-ed together.
+            Can not use in combination with `ignore_segments`
+            argument.
+        ignore_segments -- A comma-separated string of segments to
+            which learners should NOT belong.  Semgents are "OR"-ed
+            together.  Can not use in combination with `segments`
+            argument.
+        cohort -- The cohort to which all returned learners must
+            belong.
+        enrollment_mode -- The enrollment mode to which all returned
+            learners must belong.
+        order_by -- The field for sorting the response.  Defaults to 'name'.
+        sort_order -- The sort direction.  One of 'asc' or 'desc'.
+            Defaults to 'asc'.
+
+    """
+    serializer_class = LearnerSerializer
+    pagination_serializer_class = ElasticsearchDSLSearchSerializer
+    paginate_by_param = 'page_size'
+    paginate_by = learner.LEARNER_API_ROSTER_PAGE_SIZE
+    max_paginate_by = 100  # TODO -- determine
+
+    #
+    # TODO: When should we return 404 if results list is empty?
+    # e.g. what if user passes in a cohort which doesn't match anything?
+    #
+
+    def get(self, request, *args, **kwargs):
+        # Cache the querystring arguments on the view object so that
+        # we can access them in `self.get_queryset`.
+        self.text_search = request.QUERY_PARAMS.get('text_search')
+        self.segments = split_query_argument(request.QUERY_PARAMS.get('segments'))
+        self.ignore_segments = split_query_argument(request.QUERY_PARAMS.get('ignore_segments'))
+        self.cohort = request.QUERY_PARAMS.get('cohort')
+        self.enrollment_mode = request.QUERY_PARAMS.get('enrollment_mode')
+        self.order_by = request.QUERY_PARAMS.get('order_by')
+        self.sort_order = request.QUERY_PARAMS.get('sort_order')
+        return super(LearnerListView, self).get(request, *args, **kwargs)
+
+    def get_queryset(self):
+        """
+        Fetches the user list from elasticsearch.  Note that an
+        elasticsearch_dsl `Search` object is returned, not an actual
+        queryset.
+        """
+        try:
+            return RosterEntry.get_users_in_course(
+                self.course_id,
+                segments=self.segments,
+                ignore_segments=self.ignore_segments,
+                cohort=self.cohort,
+                enrollment_mode=self.enrollment_mode,
+                text_search=self.text_search,
+                order_by=self.order_by,
+                sort_order=self.sort_order
+            )
+        except ValueError as e:
+            raise ParameterValueError(e.message)
